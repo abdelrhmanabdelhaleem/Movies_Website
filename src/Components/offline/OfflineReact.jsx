fe1b339cd74f0b94c2b80559f9b66ef867af225c
abdelrhmanabdelhaleem <abdelrhmman123@gmail.com>
2024-03-29 01:26:51 +0200

    change image

--- src/Components/offline/OfflineReact.jsx
+++ src/Components/offline/OfflineReact.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import img1 from "./../../images/Screenshot 2024-03-28 221943.png";
+import img1 from "./Screenshot 2024-03-28 221943.png";
 
 export default function OfflineReact() {
   return (
     <>
       <div className="d-flex  vh-100 justify-content-center align-items-center ">
         <div>
-          <img src={img1} alt="offline" className=" w-100 px-2 m-auto" />
+          <img
+            src={img1}
+            alt="You Are Offline"
+            className=" w-100 px-2 m-auto"
+          />
         </div>
       </div>
     </>
